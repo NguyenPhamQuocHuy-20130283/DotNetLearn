c0b1940834cad65b30fbced3611f78b7767d75a0
NguyenPhamQuocHuy-20130283 <20130291@st.hcmuaf.edu.vn>
2024-09-05 21:45:01 +0700

    Initial commit

---
+++ api/models/Comment.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Tittle { get; set; } = "";
+        public string Content { get; set; } = "";
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? StockId { get; set; }
+        public Stock? Stock { get; set; }
+    }
+}
